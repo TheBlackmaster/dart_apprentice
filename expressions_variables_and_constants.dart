61da1ca0c586c401a9034636f89f08d7c967c7e7
Moses <bigdang42@gmail.com>
2025-08-04 19:21:12 +0100

    mini exercises 2

--- expressions_variables_and_constants.dart
+++ expressions_variables_and_constants.dart
@@ -1,13 +1,14 @@
 // Mini-exercise
-//Arithmetics operation mini-exercise
-// Print the value of 1 over the square root of 2 in Dart. 
-// Confirm that it equals the sine of 45°.
 
 import 'dart:math';
 
 void main() {
-// 1 over the square root of 2
+//Arithmetics operation mini-exercise
 
+// Print the value of 1 over the square root of 2 in Dart. 
+// Confirm that it equals the sine of 45°.
+
+// 1 over the square root of 2
 double oneOverSqrt2 = 1/sqrt(2);
 print(oneOverSqrt2);
 
@@ -21,4 +22,30 @@ if (oneOverSqrt2 == sineOf45) {
 } else {
   print("Values are not equal");
 }
+
+
+// Mini-exercises
+// 1. Declare a constant of type int called myAge and set it to your age.
+// 2. Declare av ariable of type double called averageAge.
+// Initially,set the variable to your own age. 
+// Then, set it to the average of your age and your best friend’s age.
+// 3. Create a constant called testNumber and initialize it with whatever integer you’d like. 
+// Next, create another constant called evenOdd and set it equal to testNumber modulo 2. 
+// Now change testNumber to various numbers. What do you notice about evenOdd?
+
+int myAge = 25; // Declare a constant of type int called myAge and set it to your age.
+double averageAge = 25; // Declare a variable of type double called averageAge. Initially, set the variable to your own age.
+averageAge = (myAge + 24)/2; // Set it to the average of your age and your best friend’s age (assuming your best friend is 24 years old).
+
+print(averageAge);
+
+const testNumber = 36;
+const evenOdd = testNumber % 2;
+
+print(evenOdd);
+
+// when i channge testNumber to various numbers, evenOdd is always 0 or 1.
+// If testNumber is even, evenOdd is 0. If testNumber is odd,
+// evenOdd is 1. This is because the modulo operator (%) returns the remainder of the
+// division of testNumber by 2. If the remainder is 0, the number is even; if the remainder is 1, the number is odd.
 }
