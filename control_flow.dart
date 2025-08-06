8c8a27e3be7e001e2eb46d6db27cd9d9f513693a
Moses <bigdang42@gmail.com>
2025-08-06 15:30:41 +0100

    switch and enums lesson

--- control_flow.dart
+++ control_flow.dart
@@ -6,8 +6,13 @@
 // 2. Use a ternary conditional operator to replace the else-ifstatement 
 // that you used above. Set the result to a variable named answer.
 
-
+enum AudioState {
+  playing,
+  paused,
+  stopped,
+}
 void main() {
+// if else-if statement and ternary conditional operator
   const myAge = 25;
   if(myAge >= 13 && myAge <= 19){
     print('Teenager');
@@ -20,5 +25,31 @@ const secondAge = 15;
 final answer = (secondAge >= 13 && secondAge <= 19) ? 'Teenager' : 'not a teenager';
 
 print(answer);
+
+
+
+
+
+/////////////////////
+// switch statements and enums
+
+// Mini-exercises
+// 1. Make an enum called AudioState and give it values to represent 
+// playing, paused and stopped states.
+// 2. Create a constant called audioState and give it an AudioState value. 
+// Write a switch statement that prints a message based on the value.
+
+const audioState = AudioState.playing;
+switch (audioState) {
+  case AudioState.playing:
+    print('Audio is playing');
+    break;
+  case AudioState.paused:
+    print('Audio is paused');
+    break;
+  case AudioState.stopped:
+    print('Audio is stopped');
+    break;
 }
 
+}
