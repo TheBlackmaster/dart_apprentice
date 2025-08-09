a4f9cdd8ddfc5053b4c300e5efad973bf31c6183
Moses <bigdang42@gmail.com>
2025-08-09 01:07:30 +0100

    Add function youAreWonderful with named parameters for personalized messages

---
+++ function.dart
@@ -0,0 +1,21 @@
+// In this fill we will learn about functions in Dart.
+
+void main(){
+// Mini-exercises
+// 1. Write a function named youAreWonderful,with a String parameter called name. 
+//    It should return a string using name, and say something like 
+//    “You’re wonderful, Bob.”
+// 2. Add another int parameter to that function called numberPeople 
+//    so that the function returns something like “You’re wonderful, Bob.
+//    10 people think so.”
+// 3. Make both inputs named parameters.
+//    Make name required and set numberPeople to have a default of 30.
+
+
+String youAreWonderful({required String? name, int numberPeople = 30}){
+  return 'You are wonderful $name. $numberPeople people think so.';
+}
+
+print(youAreWonderful(name:'Doat', numberPeople:10000));
+}
+
