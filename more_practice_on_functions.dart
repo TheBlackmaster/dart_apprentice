faaf489c2c047273666a1a2b7b6c13b7ba91f949
Moses <bigdang42@gmail.com>
2025-08-13 00:25:26 +0100

    optional parameters

--- more_practice_on_functions.dart
+++ more_practice_on_functions.dart
@@ -1,6 +1,10 @@
 void main(){
  findPerimiter(2, 100);
  print(getArea(2,100));
+
+ printCities('jos', 'mangu', 'kano');
+
+ printCountries('Nigeria', 'Zimbabwe');
 }
 
 void findPerimiter(int length,int width) {
@@ -11,4 +15,19 @@ void findPerimiter(int length,int width) {
 
 String getArea(int length, int width) {
   return 'Area of the rectangle is: ${length * width}';
+}
+
+// Required Parameters
+void printCities(String city1, String city2, String city3) {
+  print('City 1: $city1');
+  print('City 2: $city2');
+  print('City 3: $city3');
+}
+
+// Optional Positional Parameters
+
+void printCountries(String country1, [String? country2,String? country3] ) {
+  print('City 1: $country1');
+  print('City 2: $country2');
+  print('City 3: $country3');
 }
