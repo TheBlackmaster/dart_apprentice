ef3bf65ada5fc1cd120b556e9e4db31b90303c10
Moses <bigdang42@gmail.com>
2025-08-13 23:54:00 +0100

    variables, data types, strings and interpolation

---
+++ deepseek_practice.dart
@@ -0,0 +1,36 @@
+void main(){
+// More Practice
+
+// 1.1
+var name = 'Moses Adang';
+int age = 26;
+double height = 1.6;
+bool isStudent = false;
+
+print('Hi. My name is $name and I $age years old. I am $height m in height and whether I am a student is $isStudent');
+
+// 1.2
+const pi = 3.14159;
+const r = 5;
+final areaCircle = pi * (r * r);
+
+print('The area of the circle is ${areaCircle}cm2');
+
+// 2.1
+var value = 'Dart Programming';
+print(value.length);
+
+// Print it in uppercase and lowercase
+print(value.toLowerCase());
+print(value.toUpperCase());
+// Check if it contains the word "Programming"
+print(value.contains('Programming'));
+
+
+// 2.2
+var firstName = 'Moses';
+var lastName = ' Azi-Gwom';
+
+final fullName = firstName + lastName;
+print('Hello, $fullName');
+}
