3c39480d87c098fd3a0f812a23d530de693dc6a8
Moses <bigdang42@gmail.com>
2025-08-13 14:09:14 +0100

    Even/odd numbers, sum of number funcions and guess the number

--- practice_exercises.dart
+++ practice_exercises.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main(){
   // 
   // Personal Info Card
@@ -19,4 +21,55 @@ void main(){
   for(int i = 1; i <=12; i++){
     print('$num x $i = ${num * i}');
   }
+
+
+  //  
+// Even and odd numbers
+for(int i = 1; i <=20; i++){
+  // print(i);
+  
+  if(i % 2 == 0){
+    print('$i is an even number');
+  } else{
+    print('$i is an odd number');
+  }
+}
+
+//
+// Sum of numbers function
+print(sumNumbers(1,55)); // this is the called function
+
+// 
+// Guess the number
+
+final secretNumber = Random().nextInt(4);
+int guess = 3;
+
+print(secretNumber);
+print(guess);
+
+
+while(guess != secretNumber){
+  if(guess < secretNumber){
+    print('too low');
+  } else if(guess > secretNumber){
+    print('too high');
+  } else{
+    print('error');
+  }
+  
+  break;
+}
+
+  if(guess == secretNumber){
+    print('there we have it');
+  }
+}
+
+int sumNumbers(int start, int end){
+  int sum = 0;
+  for(int i = start; i <= end; i++){
+    sum += i;
+  }
+  return sum;
 }
