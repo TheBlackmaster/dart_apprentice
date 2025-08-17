7023174c0507dedab18824d283d86553442d4dc8
Moses <bigdang42@gmail.com>
2025-08-17 22:28:25 +0100

    password checker

--- classes2.dart
+++ classes2.dart
@@ -5,7 +5,15 @@ final user = User()
 
 print(user);
 print(user.toJson());
+
+Password password = Password()
+        ..value = '308403';
+        
+  print(password);
+  print(password.isValid());
 }
+
+
 
 class User{
   int id = 0;
@@ -18,5 +26,22 @@ String toJson() {
  @override
   String toString(){
     return 'User: id = $id, name = $name';
+  }
+}
+
+
+
+
+
+class Password{
+  String value = '';
+  
+  bool isValid(){
+    return value.length > 8;
+  }
+  
+  @override
+  String toString(){
+    return value;
   }
 }
